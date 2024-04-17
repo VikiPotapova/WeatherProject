ce400d2d03d939610c32fc0a5ab79f4ac2b023c9
VikiPotapova <viki.patapava@gmail.com>
2024-04-17 14:09:07 +0300

    added h2 db.

--- src/main/java/com/example/weatherproject/domain/Weather.java
+++ src/main/java/com/example/weatherproject/domain/Weather.java
@@ -1,11 +1,20 @@
 package com.example.weatherproject.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Entity
 public class Weather {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String city;
     private Integer temperature;
     private LocalDateTime localDateTime;
