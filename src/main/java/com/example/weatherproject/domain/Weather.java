76a50a227805c6dadd010b47fe7fb5ed64e94b2a
VikiPotapova <viki.patapava@gmail.com>
2024-04-18 12:02:20 +0300

    added column constraints

--- src/main/java/com/example/weatherproject/domain/Weather.java
+++ src/main/java/com/example/weatherproject/domain/Weather.java
@@ -1,5 +1,6 @@
 package com.example.weatherproject.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,7 +16,13 @@ public class Weather {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String city;
+
+    @Column(nullable = false)
     private Integer temperature;
+
+    @Column(nullable = false)
     private LocalDateTime localDateTime;
 }
