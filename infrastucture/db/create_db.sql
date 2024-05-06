524dc52adfe8ac026fbe19e79651664a563f7591
VikiPotapova <viki.patapava@gmail.com>
2024-05-06 11:04:04 +0300

    added docker-compose file

---
+++ infrastucture/db/create_db.sql
@@ -0,0 +1,1 @@
+create database weather_db
--- src/main/java/com/example/weatherproject/WeatherProjectApplication.java
+++ src/main/java/com/example/weatherproject/WeatherProjectApplication.java
@@ -1,11 +1,16 @@
 package com.example.weatherproject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class WeatherProjectApplication {
 
     public static void main(String[] args) {
